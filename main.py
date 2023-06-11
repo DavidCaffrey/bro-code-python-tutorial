0bf2cfb3729007c85201caa93698f85a2ec1e08b
David Caffrey <dave.caffrey71@gmail.com>
2023-06-11 13:32:55 +0100

    next commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# name = "dave"
+# number1 =23
+# print(name + str(number1))
+# print(str(number1))
+# print(number1)
+# print(type(number1))
+
+# age_two = "52"
+# age = 52
+# print(int(age_two))
+
+
+# cost = 12.33
+# print(type(cost))
+
+# isHuman = True
+
+# print(str(type(isHuman)) + str(isHuman))
+
+#  MULTIPlE ASSIGNMENT
+
+# name,age,isAttractive = "dave",52,True
+
+# spongeBob = doormate = tomtom = fortran = 30
+# print(name)
+# print(doormate)
+
+
+# STRING OPERATIONS
+
+# name = " david caffrey "
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.capitalize())
+# capitalize both
+# print(name.title())
+# print(name.strip())
+# print(name.rstrip())
+# print(name.lstrip())
+# print(name.find("c"))
+# check if string is all alphanumeric
+# print(name.isalpha())
+# print(name.isalnum())
+# print(name.replace("a", "e"))
+# print(name*20)
+
+# TYPE CASTING
+
+# x= 15.5602
+# print(int(x))
+# print(x)
+# x = int(x)
+# print(x)
+
+# ans = int(input("Please enter a name:  "))
+# print("Hello " + str(ans))
+# print(ans*2)
+
+
+# import math
+
+# pi = 3.1415926535897932
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.pow(pi,4))
+# print(math.log(pi,4))
+
